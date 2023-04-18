e39bc6fc235ea27ca082669229ef577fc2c145fb
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-04-18 16:36:48 -0300

    par e impar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    var countP = 0;
+    var countI = 0;
+    val array: Array<Int> = arrayOf(2, 30, 3, 5, 7, 8, 9, 10, 4, 44, 55, 70)
+    for (indice in array) {
+        if (indice % 2 == 0) {
+            countP++
+            println("$indice é par")
+        } else {
+            countI++
+            println("$indice é impar")
+        }
+    }
+    println("De ${array.size} elementos temos $countP numeros pares")
+    println("De ${array.size} elementos temos $countI numeros impares")
 }
